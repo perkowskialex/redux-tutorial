c5f532d5b1c9a05c864e2f2c2e360d8180148ecf
Alex Perkowski <perkowskialex@gmail.com>
2020-03-05 17:14:24 -0600

    connect visible todolist

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,3 +1,7 @@
+import { toggleTodo } from "../redux/actions";
+import { connect } from "react-redux";
+import TodoList from "../components/TodoList";
+
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
     case "SHOW_COMPLETED":
@@ -15,3 +19,15 @@ const mapStateToProps = state => {
     todos: getVisibleTodos(state.todos, state.visibilityFilter)
   };
 };
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTodoClick: id => {
+      dispatch(toggleTodo(id));
+    }
+  };
+};
+
+const VisibleTodoList = connect(mapStateToProps, mapDispatchToProps)(TodoList);
+
+export default VisibleTodoList;
